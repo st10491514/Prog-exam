156dbc5e7c3aa328a3da767753f92b016d8cff25
st10491514 <ST10491514@rcconnect.edu.za>
2025-11-10 13:27:53 +0200

    Add iProductSales interface for product sales methods

---
+++ Q2iProductSales.java
@@ -0,0 +1,8 @@
+public interface iProductSales {
+        int[][] GetProductsales();
+        int GetTotalSales();
+        int GetSalesOverLimit();
+        int GetSalesUnderLimit();
+        int GetProductsProcessed();
+        double GetAverageSales();
+    }
